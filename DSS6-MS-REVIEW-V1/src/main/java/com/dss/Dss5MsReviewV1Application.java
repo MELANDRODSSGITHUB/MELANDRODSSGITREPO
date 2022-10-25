7ff88e4c4d384197a57a4f1959e477323bad204a
MELANDRODSSGITHUB <melandro.richardson.jr@gmail.com>
2022-10-25 12:45:06 +0800

    Creating enterprise project modules

---
+++ DSS6-MS-REVIEW-V1/src/main/java/com/dss/Dss5MsReviewV1Application.java
@@ -0,0 +1,13 @@
+package com.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss5MsReviewV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dss5MsReviewV1Application.class, args);
+    }
+
+}
